8f0188133b7a5f83ec38821bc58ca326b510568b
hritikKumar9412 <hritikthakur200@gmail.com>
2026-07-08 16:58:58 +0530

    Add Book design implementation

---
+++ Design/Maintest.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Maintest {
+    public static void main(String[] args) {
+        // creating the book object
+        Book book = new Book("Java","Sumicro","Java is a powerful language");
+
+        // create printer object
+
+        BookPrinter printer = new BookPrinter();
+
+        // Display Book details
+        System.out.println();
+
+
+    }
+}
